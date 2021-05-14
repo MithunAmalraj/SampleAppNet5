424cbce1670e6c3f557f2da82345c84e4be78030
Mithun <mithun.amalraj@gmail.com>
2021-05-14 12:49:02 +0530

    Add files via upload

---
+++ SampleApplication/SampleApplication/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SampleApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Net;
+using System.Net.Security;
+
+namespace SampleApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(delegate { return true; });    //	 Could Not Establish Trust Relationship For The SSL/ TLS Secure Channel
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            DataTable objresutl = new DataTable();
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection("Data Source=DESKTOP-6SF7QF1;Initial Catalog=Test;Integrated Security=SSPI;"))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand("procedure_name", myCon))
+                {
+                    myCommand.CommandType = CommandType.StoredProcedure;
+                    myReader = myCommand.ExecuteReader();
+                    objresutl.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
